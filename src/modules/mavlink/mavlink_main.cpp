27abad41baa92fd9c3f6a1ccad4abe7b23851e43
Sarthak Kaingade <sarthakkaingade@yahoo.com>
2015-01-20 11:17:59 +0530

    Enabled onboard mavlink

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1415,6 +1415,7 @@ Mavlink::task_main(int argc, char *argv[])
 		configure_stream("GLOBAL_POSITION_INT", 50.0f);
 		configure_stream("CAMERA_CAPTURE", 2.0f);
 		configure_stream("ATTITUDE_TARGET", 10.0f);
+		configure_stream("DISTANCE_SENSOR", 25.0f);
 		configure_stream("POSITION_TARGET_GLOBAL_INT", 10.0f);
 		configure_stream("VFR_HUD", 10.0f);
 		break;
